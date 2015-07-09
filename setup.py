201d7e79521f346142148841f99f519071172fb1
Chris Adams <chris@improbable.org>
2015-07-09 10:47:30 -0400

    v0.2.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='phash',
-    version='0.1.0',
+    version='0.2.0',
     description='ctypes interface to libphash',
     long_description=readme + '\n\n' + history,
     author='Chris Adams',
