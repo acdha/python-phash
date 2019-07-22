15f681d1073046c88b4b12cf337f5cf12ea6c8dd
Chris Adams <cadams@loc.gov>
2019-07-22 14:40:20 -0400

    Start adding some public tests

--- tests/test_phash.py
+++ tests/test_phash.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-test_phash
-----------------------------------
-
-Tests for `phash` module.
-"""
-
+import os
 import unittest
 
-from phash import phash
+import phash
+
+TEST_IMAGES = {
+    "simple": [
+        os.path.join(os.path.dirname(__file__), "images", "simple-test-1.png"),
+        os.path.join(os.path.dirname(__file__), "images", "simple-test-2.png"),
+    ]
+}
 
 
 class TestPhash(unittest.TestCase):
-    def setUp(self):
-        pass
+    def test_digest(self):
+        self.assertIsNone(None, phash.image_digest(TEST_IMAGES["simple"][0]))
 
-    def test_something(self):
-        pass
+    def test_correlation(self):
+        digest_1 = phash.image_digest(TEST_IMAGES["simple"][0])
+        digest_2 = phash.image_digest(TEST_IMAGES["simple"][1])
 
-    def tearDown(self):
-        pass
+        pcc = phash.cross_correlation(digest_1, digest_2)
+
+        self.assertAlmostEqual(0, pcc)
 
 
 if __name__ == "__main__":
